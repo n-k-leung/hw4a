fca868bbb805a4c3344fb1d765b956ba7f42d33b
natalie <natalie.ky.leung@gmail.com>
2023-04-29 22:13:15 -0700

    fixed

--- merge.py
+++ merge.py
@@ -1,18 +1,17 @@
 def merge(l1,l2):
-        size1 = len(l1)
-        size2 = len(l2)
-        merged = []
-        i = 0
-        j = 0
-        while i < size1 and j < size2:
+
+     merged = []
+     i = 0
+     j = 0
+     while i < len(l1) and j < len(l2):
                 if l1[i] <= l2[i]:
                         merged.append(l1[i])
                         i = i + 1
                 else:
                         merged.append(l2[j])
                         j = j + 1
-        if(i<size1):
+        if(i<len(l1)):
                 merged.extend(l1[i:])
-        if(j<size2):
+        if(j<len(l2)):
                 merged.extend(l2[j:])
         return merged
